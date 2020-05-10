1badceb085ae7fd58e1e100844901a3c0bf171ae
Andrei <andrei@thebrandwebbers.com>
2020-05-10 18:29:23 +0300

    prima versiune de aplicatie incarcata, cu ecranele de homepage

---
+++ app/src/main/java/com/sia5/appdrawer/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sia5.appdrawer;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new CustomPagerAdapter(this));
+    }
+}
